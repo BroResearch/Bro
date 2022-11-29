0e3d83575d825089bb5ff1031add963579501598
Daniel <daniel.noguera.7e5@itb.cat>
2022-11-29 20:16:07 +0100

    Commit Daniel Noguera

--- src/dao/Kweets.kt
+++ src/dao/Kweets.kt
@@ -11,6 +11,5 @@ object Kweets : Table() {
     val user = varchar("user_id", 20).index()
     val date = datetime("date")
     val replyTo = integer("reply_to").index().nullable()
-    val directReplyTo = integer("direct_reply_to").index().nullable()
     val text = varchar("text", 1024)
 }
---
+++ src/main.kt
@@ -0,0 +1,3 @@
+package io.ktor.samples.kweet
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
